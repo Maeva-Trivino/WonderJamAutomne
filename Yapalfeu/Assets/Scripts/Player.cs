0447f87fee81beb0f703813cc02f8331375616a2
spherebleue <7spherebleue7@gmail.com>
2019-10-13 11:28:46 -0400

    more time

--- Yapalfeu/Assets/Scripts/Player.cs
+++ Yapalfeu/Assets/Scripts/Player.cs
@@ -58,7 +58,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        seedCount = 10;
+        seedCount = 7;
         inRange = new HashSet<GameObject>();
         updatePopup(null);
         UIManager.instance.UpdateSeeds(seedCount);
