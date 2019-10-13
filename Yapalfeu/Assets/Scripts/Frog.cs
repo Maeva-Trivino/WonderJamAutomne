ce7ae820aa5bd543579e17943ede25887e58bfc9
Yoann HAFFNER <yoyofrom68@gmail.com>
2019-10-13 15:29:43 -0400

    Frog

---
+++ Yapalfeu/Assets/Scripts/Frog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        Invoke("Fire", Random.Range(3,20));
+    }
+
+    void Fire()
+    {
+        animator.Play("Show");
+        Invoke("Fire", Random.Range(3, 20));
+    }
+}
